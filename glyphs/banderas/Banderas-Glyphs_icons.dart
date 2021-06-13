0e6a278ace6249de106c8a5eb84bbc8c808ee741
hihellomonster <70384348+hihellomonster@users.noreply.github.com>
2021-06-13 00:37:01 -0500

    Add files via upload

---
+++ glyphs/banderas/Banderas-Glyphs_icons.dart
@@ -0,0 +1,23 @@
+// Place fonts/Banderas-Glyphs.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: Banderas-Glyphs
+//      fonts:
+//       - asset: fonts/Banderas-Glyphs.ttf
+import 'package:flutter/widgets.dart';
+
+class Banderas_Glyphs {
+  Banderas_Glyphs._();
+
+  static const String _fontFamily = 'Banderas-Glyphs';
+
+  static const IconData tailandia = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData argentina = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData guatemala = IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData chile = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData colombia = IconData(0xe919, fontFamily: _fontFamily);
+  static const IconData costarica = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData mexico = IconData(0xe921, fontFamily: _fontFamily);
+  static const IconData eeuu = IconData(0xe929, fontFamily: _fontFamily);
+}
