3d5494aa11233c5a578f8e9cfaa890499a918aa1
hihellomonster <70384348+hihellomonster@users.noreply.github.com>
2021-07-05 04:45:31 -0500

    Add files via upload

---
+++ raw/true/rem.js
@@ -0,0 +1,70 @@
+var myObject = {
+    "how-to": [{
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "14-agosto-2021",
+        "href": "https://hihello.monster/how-to/Tab-Pure-CSS/ ",
+        "how-to-tittle": "Tab-Pure-CSS",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "16-agosto-2021",
+        "href": "https://hihello.monster/how-to/responsive-typography-generator/",
+        "how-to-tittle": "Responsive-typography-generator",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "16-agosto-2021",
+        "href": "https://hihello.monster/how-to/simple-blog-concept-pure-modal-tab-icon-efect-css/",
+        "how-to-tittle": "Simple-blog-concept-pure-modal-tab-icon-efect-css",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "16-agosto-2021",
+        "href": "https://hihello.monster/how-to/DateTime.html ",
+        "how-to-tittle": "DateTime",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "16-agosto-2021",
+        "href": "https://hihello.monster/how-to/Expanding-Menu-Concept.html",
+        "how-to-tittle": "Expanding-Menu-Concept.",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "15-agosto-2021",
+        "href": "https://hihello.monster/how-to/Lightbox+Horizontal_Scrollable_menu.html",
+        "how-to-tittle": "Lightbox+Horizontal_Scrollable_menu",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "16-agosto-2021",
+        "href": "https://hihello.monster/how-to/modal-CSS.html",
+        "how-to-tittle": "Modal-CSS",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "16-agosto-2021",
+        "href": "https://hihello.monster/how-to/simpleModal.html",
+        "how-to-tittle": "Simple Modal",
+        "Post": "Hice tal cosa",
+        "accion": "Descargar",
+    }, {
+        "src": "https://github.com/hihellomonster/hihello/blob/master/favicon.png?raw=true",
+        "fechaDePublicacion": "",
+        "href": "",
+        "how-to-tittle": "",
+        "Post": "",
+        "accion": "Descargar",
+    }]
+};
+
+
+w3.displayObject("idPost", myObject);
+w3.displayObject("index-how-to", myObject);
