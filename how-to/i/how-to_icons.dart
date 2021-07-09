5153fc4ee110c20c4bc899ae38d6dcf88d9ece56
hihellomonster <70384348+hihellomonster@users.noreply.github.com>
2021-07-09 02:37:04 -0500

    Add files via upload

---
+++ how-to/i/how-to_icons.dart
@@ -0,0 +1,63 @@
+// Place fonts/how-to.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: how-to
+//      fonts:
+//       - asset: fonts/how-to.ttf
+import 'package:flutter/widgets.dart';
+
+class How_to {
+  How_to._();
+
+  static const String _fontFamily = 'how-to';
+
+  static const IconData chaos_dragon_ball_z = IconData(0xe9c8, fontFamily: _fontFamily);
+  static const IconData trasero_vegeta = IconData(0xea17, fontFamily: _fontFamily);
+  static const IconData amor_love_toy_story = IconData(0xea37, fontFamily: _fontFamily);
+  static const IconData logo_viajemos_todxs_por_mexico_turismo_gay_turismo_rosa_color = IconData(0xf344, fontFamily: _fontFamily);
+  static const IconData cursor_hand = IconData(0xf630, fontFamily: _fontFamily);
+  static const IconData hospedaje = IconData(0xf63d, fontFamily: _fontFamily);
+  static const IconData codepen = IconData(0xf657, fontFamily: _fontFamily);
+  static const IconData github = IconData(0xf65c, fontFamily: _fontFamily);
+  static const IconData www = IconData(0xf65f, fontFamily: _fontFamily);
+  static const IconData twitter_bird = IconData(0xf5a4, fontFamily: _fontFamily);
+  static const IconData drag_rupaul = IconData(0xea47, fontFamily: _fontFamily);
+  static const IconData viajero = IconData(0xea5d, fontFamily: _fontFamily);
+  static const IconData solicitar_transporte = IconData(0xf45f, fontFamily: _fontFamily);
+  static const IconData sexoral_one = IconData(0xf45b, fontFamily: _fontFamily);
+  static const IconData bailar = IconData(0xea60, fontFamily: _fontFamily);
+  static const IconData avatar_fuera_del_closet = IconData(0xea61, fontFamily: _fontFamily);
+  static const IconData hombre = IconData(0xea62, fontFamily: _fontFamily);
+  static const IconData corriendo = IconData(0xf094, fontFamily: _fontFamily);
+  static const IconData flecha_atras = IconData(0xf0aa, fontFamily: _fontFamily);
+  static const IconData flecha_siguiente = IconData(0xf0ab, fontFamily: _fontFamily);
+  static const IconData touch_app = IconData(0xf56c, fontFamily: _fontFamily);
+  static const IconData tarjeta_de_regalo = IconData(0xf463, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xf0b4, fontFamily: _fontFamily);
+  static const IconData avatar_hihello = IconData(0xec41, fontFamily: _fontFamily);
+  static const IconData pokebola = IconData(0xf413, fontFamily: _fontFamily);
+  static const IconData logo_hihello = IconData(0xea51, fontFamily: _fontFamily);
+  static const IconData xiiber = IconData(0xf0b5, fontFamily: _fontFamily);
+  static const IconData mascota = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData dragon_ball_burger = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData senzubucks_coffee = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData corazon_doble = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData netflix_sexflix = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData masturbate = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData gato = IconData(0xec5f, fontFamily: _fontFamily);
+  static const IconData luna_pokemon = IconData(0xea48, fontFamily: _fontFamily);
+  static const IconData sol_pokemon = IconData(0xea4b, fontFamily: _fontFamily);
+  static const IconData yas_queen = IconData(0xea5f, fontFamily: _fontFamily);
+  static const IconData flecha_atras1 = IconData(0xf0a8, fontFamily: _fontFamily);
+  static const IconData flecha_siguiente1 = IconData(0xf0a9, fontFamily: _fontFamily);
+  static const IconData lavarse_las_manos = IconData(0xf0c1, fontFamily: _fontFamily);
+  static const IconData enjabonarse_las_manos = IconData(0xf09c, fontFamily: _fontFamily);
+  static const IconData secarse_las_manos = IconData(0xf45a, fontFamily: _fontFamily);
+  static const IconData touch_app1 = IconData(0xf56b, fontFamily: _fontFamily);
+  static const IconData copiar = IconData(0xf093, fontFamily: _fontFamily);
+  static const IconData guardar = IconData(0xf0b2, fontFamily: _fontFamily);
+  static const IconData menu_puntos = IconData(0xf3ac, fontFamily: _fontFamily);
+  static const IconData menu = IconData(0xf3ab, fontFamily: _fontFamily);
+  static const IconData home1 = IconData(0xf0b3, fontFamily: _fontFamily);
+}
