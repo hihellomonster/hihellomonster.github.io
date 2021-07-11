db6ee0c482327dc1d68a2a08a3528c07eb4b8d23
hihellomonster <70384348+hihellomonster@users.noreply.github.com>
2021-07-11 17:53:49 -0500

    Add files via upload

--- luisangel.maciel/lib/luisangelmaciel_icons.dart
+++ luisangel.maciel/lib/luisangelmaciel_icons.dart
@@ -12,6 +12,10 @@ class Luisangelmaciel {
 
   static const String _fontFamily = 'luisangelmaciel';
 
+  static const IconData ico_codepen = IconData(0xe92a, fontFamily: _fontFamily);
+  static const IconData ico_codesandbox = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData ico_replit = IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData w3 = IconData(0xe931, fontFamily: _fontFamily);
   static const IconData avatar_hihello = IconData(0xec41, fontFamily: _fontFamily);
   static const IconData luna_pokemon = IconData(0xea48, fontFamily: _fontFamily);
   static const IconData sol_pokemon = IconData(0xea4b, fontFamily: _fontFamily);
@@ -40,24 +44,23 @@ class Luisangelmaciel {
   static const IconData intagram_camera = IconData(0xf66c, fontFamily: _fontFamily);
   static const IconData pinterest = IconData(0xe92d, fontFamily: _fontFamily);
   static const IconData send = IconData(0xe92e, fontFamily: _fontFamily);
-  static const IconData codepen2 = IconData(0xf659, fontFamily: _fontFamily);
   static const IconData repliit = IconData(0xf65a, fontFamily: _fontFamily);
   static const IconData codesandbox = IconData(0xf65b, fontFamily: _fontFamily);
   static const IconData w3spaces = IconData(0xf663, fontFamily: _fontFamily);
   static const IconData hi5 = IconData(0xf662, fontFamily: _fontFamily);
   static const IconData hhm = IconData(0xea54, fontFamily: _fontFamily);
+  static const IconData xiib = IconData(0xf0b5, fontFamily: _fontFamily);
   static const IconData logo_viajemos_todxs_por_mexico_turismo_gay_turismo_rosa_color = IconData(0xf344, fontFamily: _fontFamily);
   static const IconData cursor_hand = IconData(0xf630, fontFamily: _fontFamily);
   static const IconData hospedaje = IconData(0xf63d, fontFamily: _fontFamily);
   static const IconData codepen = IconData(0xf657, fontFamily: _fontFamily);
-  static const IconData repliit1 = IconData(0xf66f, fontFamily: _fontFamily);
   static const IconData github_logo = IconData(0xf671, fontFamily: _fontFamily);
   static const IconData github = IconData(0xf65c, fontFamily: _fontFamily);
-  static const IconData w3spaces1 = IconData(0xf672, fontFamily: _fontFamily);
   static const IconData www = IconData(0xf65f, fontFamily: _fontFamily);
   static const IconData twitter_bird = IconData(0xf5a4, fontFamily: _fontFamily);
   static const IconData flecha_atras = IconData(0xf0a8, fontFamily: _fontFamily);
   static const IconData flecha_siguiente = IconData(0xf0a9, fontFamily: _fontFamily);
+  static const IconData etiquetas_codigo = IconData(0xf09f, fontFamily: _fontFamily);
   static const IconData touch_app = IconData(0xf56b, fontFamily: _fontFamily);
   static const IconData tarjeta_de_regalo = IconData(0xf463, fontFamily: _fontFamily);
   static const IconData home = IconData(0xf0b3, fontFamily: _fontFamily);
