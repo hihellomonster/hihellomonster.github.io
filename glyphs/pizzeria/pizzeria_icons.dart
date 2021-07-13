1ca4b6a894127bc580d6816c6c21009f9c0ec738
hihellomonster <70384348+hihellomonster@users.noreply.github.com>
2021-07-13 02:29:53 -0500

    Add files via upload

---
+++ glyphs/pizzeria/pizzeria_icons.dart
@@ -0,0 +1,31 @@
+// Place fonts/pizzeria.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: pizzeria
+//      fonts:
+//       - asset: fonts/pizzeria.ttf
+import 'package:flutter/widgets.dart';
+
+class Pizzeria {
+  Pizzeria._();
+
+  static const String _fontFamily = 'pizzeria';
+
+  static const IconData iconografia_pizzeria = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_caja_de_pizza = IconData(0xe942, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_de_pizza = IconData(0xe943, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_masa_de_pizza = IconData(0xe944, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_me_gusta = IconData(0xe945, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_pago_con_terminal = IconData(0xe946, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_pala = IconData(0xe95b, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_pedidos_a_domicilio = IconData(0xe95c, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_pizza = IconData(0xe95d, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_pizza_al_horno = IconData(0xe968, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_preparando_pizza = IconData(0xe969, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_queso = IconData(0xe96e, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_rebanadora = IconData(0xe96f, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_rodillo = IconData(0xe972, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_salsa_de_tomate = IconData(0xe974, fontFamily: _fontFamily);
+  static const IconData iconografia_pizzeria_tomate = IconData(0xe975, fontFamily: _fontFamily);
+}
